b1a1431789145729d0a22b917870ae0e13023f88
LatisGitHub <latischannel2@gmail.com>
2022-09-25 23:31:57 +0200

    nuevo 2

---
+++ tema2/eje2.php
@@ -0,0 +1,17 @@
+<?php
+    include_once($_SERVER["DOCUMENT_ROOT"] . "/cabecera.php");
+?>
+
+      <div class="col-md-8 themed-grid-col">
+		<div class="flex-shrink-0 p-3 bg-white">
+			<?php
+				echo "prueba numero 2";
+
+			?>
+		</div>	
+	  </div>
+    </div>
+
+<?php
+    include_once("../pie.php");
+?>
