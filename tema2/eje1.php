9522325d4ad7dadaf1d6c831476529973129a429
LatisGitHub <latischannel2@gmail.com>
2022-09-26 16:05:19 +0200

    commit

--- tema2/eje1.php
+++ tema2/eje1.php
@@ -1,12 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ejercicio 1</title>
 </head>
+
 <body>
-    Ejercicio 2 del tema 2, sin estilos
+    <?php
+    //Partiendo de 2 variables $primera y $segunda con valores aleatorios, hacer una página PHP que calcule y muestre por 
+    //pantalla: 
+    // - la diferencia de $primera menos $segunda 
+    // - la división de $primera entre $segunda 
+    //  Añade un comentario que 
+    //explique la función de generar números aleatorios.
+    $primera = rand(1,10);
+    $segunda = rand(1,10);
+    $diferencia= $primera - $segunda;
+    $division = $primera / $segunda;
+    echo "La diferencia entre $primera y $segunda es de: $diferencia" . "<br>";
+    echo "La division entre $primera y $segunda es de: $division";
+
+
+
+    ?>
 </body>
+
 </html>
