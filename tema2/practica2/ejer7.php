68bdb8f24315a8b2e51e649325d622c9dad0b720
LatisGitHub <latischannel2@gmail.com>
2022-10-08 21:51:43 +0200

    ejer 7 titulo

--- tema2/practica2/ejer7.php
+++ tema2/practica2/ejer7.php
@@ -8,31 +8,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <script src="./js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     <title>ejer7</title>
 </head>
 
 <body>
     <div class="container">
 
+        <h1>Bienvenido a tu tienda de libros</h1>
 
         <?php
-        /*
-    Vamos a crear una librería online, pero con los libros almacenados en un array asociativo. Los datos 
-    a guardar por cada libro son: ISBN, título, descripción, categoría, editorial, foto, precio. - La categoría puede ser: 
-    ciencias, cocina, deporte, novela, historia, scifi, negra, romántica. - El campo foto será una url en tu pc en la misma 
-    carpeta que esté el fichero php nos crearemos una carpeta imgs donde meteremos las imágenes de cada libro llamándolas 
-    con el isbn.jpg. El tamaño de cada imagen será aproximadamente 100x150px. - ISBN será un número de trece dígitos que se 
-    puede tratar como una cadena. - Precio será un float con dos decimales.
-    A continuación, te muestro cómo debería quedar la visualización del array. 
-    Tienes que intentar que sea lo más parecido posible. Como mínimo deberás tener 15 libros, y mostrar 4 libros en cada fila. 
-    Los datos e imágenes de los libros deben ser lo más reales posible. Debes tener como mínimo 5 libros de novela histórica 
-    y 5 de novela negra, pero sólo mostraremos los 4 primeros de cada una de esas categorías.*/
         function pintarPorCategoria($productos, $categoria)
         {
             echo "<h3>" . strtoupper($categoria) . "</h3>";
             $cont = 0;
             foreach ($productos as $valor) {
-
                 if ($valor['categoria'] == $categoria) {
 
                     if ($cont == 4)
@@ -40,11 +32,11 @@
                     $cont++;
 
                     echo "<div class='card mb-5' style='width: 16rem;'>
-                        <img src='" . $valor["imagen"] . "' class='card-img-top' alt='...'>
+                        <img src='" . $valor["imagen"] . "' class='card-img-top' alt='...' width=50% height=50%>
                             <div class='card-body'>
-                            <h5 class='card-title'>" . $valor["nombre"] . "</h5>
+                            <h5 class='card-title'>" . $valor["titulo"] . "</h5>
+                            <p class='card-text'>" . $valor['ISBN'] . "</p>
                             <p class='card-text'>" . $valor['descripcion'] . "</p>";
-
                     echo "
                             <p class='card-text'><small class='text-secondary'>" . $valor["precio"] . " €</small></p>
 
@@ -56,59 +48,33 @@
         }
 
 
-
-        $carrito = array(
-            array("ISBN" => 1234, "titulo" => "libro1", "descripcion" => "descripcion1", "categoría" => "terror", "editorial" => "A", "foto" => " ", "precio" => 38.20),
-            array("ISBN" => 4567, "titulo" => "libro2", "descripcion" => "descripcion2", "categoría" => "ciencia", "editorial" => "B", "foto" => " ", "precio" => 45.50),
-            array("ISBN" => 7891, "titulo" => "libro3", "descripcion" => "descripcion3", "categoría" => "terror", "editorial" => "C", "foto" => " ", "precio" => 50.20)
+        //Productos de una tienda
+        $productos = array(
+            array("ISBN" => 1234, "titulo" => "libro1", "descripcion" => "descripcion1", "categoria" => "terror", "editorial" => "A", "imagen" => "img/libro2.jpg", "precio" => 38.20),
+            array("ISBN" => 4567, "titulo" => "libro2", "descripcion" => "descripcion2", "categoria" => "ciencia", "editorial" => "B", "imagen" => "img/libro2.jpg", "precio" => 45.50),
+            array("ISBN" => 7891, "titulo" => "libro3", "descripcion" => "descripcion3", "categoria" => "terror", "editorial" => "C", "imagen" => "img/libro3.jpg ", "precio" => 50.20),
+            array("ISBN" => 4567, "titulo" => "libro2", "descripcion" => "descripcion2", "categoria" => "ciencia", "editorial" => "B", "imagen" => "img/libro2.jpg", "precio" => 45.50),
+            array("ISBN" => 4567, "titulo" => "libro2", "descripcion" => "descripcion2", "categoria" => "ciencia", "editorial" => "B", "imagen" => "img/libro2.jpg", "precio" => 45.50),
+            array("ISBN" => 1234, "titulo" => "libro1", "descripcion" => "descripcion1", "categoria" => "terror", "editorial" => "A", "imagen" => "img/libro2.jpg", "precio" => 38.20),
+            array("ISBN" => 4567, "titulo" => "libro2", "descripcion" => "descripcion2", "categoria" => "ciencia", "editorial" => "B", "imagen" => "img/libro2.jpg", "precio" => 45.50),
+            array("ISBN" => 7891, "titulo" => "libro3", "descripcion" => "descripcion3", "categoria" => "terror", "editorial" => "C", "imagen" => "img/libro3.jpg ", "precio" => 50.20),
+            array("ISBN" => 4567, "titulo" => "libro2", "descripcion" => "descripcion2", "categoria" => "ciencia", "editorial" => "B", "imagen" => "img/libro2.jpg", "precio" => 45.50),
+            array("ISBN" => 1234, "titulo" => "libro1", "descripcion" => "descripcion1", "categoria" => "terror", "editorial" => "A", "imagen" => "img/libro2.jpg", "precio" => 38.20),
+            array("ISBN" => 4567, "titulo" => "libro2", "descripcion" => "descripcion2", "categoria" => "ciencia", "editorial" => "B", "imagen" => "img/libro2.jpg", "precio" => 45.50),
+            array("ISBN" => 7891, "titulo" => "libro3", "descripcion" => "descripcion3", "categoria" => "terror", "editorial" => "C", "imagen" => "img/libro3.jpg ", "precio" => 50.20),
+            array("ISBN" => 4567, "titulo" => "libro2", "descripcion" => "descripcion2", "categoria" => "ciencia", "editorial" => "B", "imagen" => "img/libro2.jpg", "precio" => 45.50)
         );
-
-        //Pintar productos
-        echo "<table class='table table-striped table-hover'>";
-        echo "<thead class='text-primary'>";
-        echo "  <tr>";
-        //Sacamos el nombre de cada columna con array_keys del primer array (producto)
-        foreach (array_keys($carrito[0]) as $valor)
-            echo "<td>" . strtoupper($valor) . "</td>";
-        echo "  </tr>";
-        echo "</thead>";
-        echo "<tbody>";
-        foreach ($carrito as $valor) {
-            echo "<tr>";
-
-            foreach ($valor as $campo) {
-                echo "<td>";
-                echo $campo;
-                echo "</td>";
-            }
-
-            echo "</tr>";
-        }
-        echo "</tbody>";
-        echo "</table>";
-
-        echo "<br><br>";
-
-
 
         echo "<div class='container'>";
         echo "<div class='row'>";
 
+        //Me quedo con los valores de la columna categoría, y los valores los meto en un array
+        $categorias = array_column($productos, "categoria");
+        //Quito repetidos
+        $categorias = array_unique($categorias);
 
-        foreach ($carrito as $valor) {
-
-
-            echo "<div class='card' style='width: 16rem;'>
-                  <img src='" . $valor["imagen"] . "' class='card-img-top' alt='...'>
-                      <div class='card-body'>
-                      <h5 class='card-title'>" . $valor["nombre"] . "</h5>
-                      <p class='card-text'>" . $valor['descripcion'] . "</p>
-                      <p class='card-text'><small class='text-secondary'>" . $valor["precio"] . " €</small></p>
-                      <a href='#' class='btn btn-primary'>Comprar</a>
-                  </div>
-              </div>";
-        }
-
+        foreach ($categorias as $categoria)
+            pintarPorCategoria($productos, $categoria);
 
         echo "</div>";
         echo "</div>";
--- tema2/practica2/ejer8.php
+++ tema2/practica2/ejer8.php
@@ -1,19 +1,107 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ejer8</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </head>
+
 <body>
-    <?php
-    /*Web videojuego. 
-    Intenta hacer con diseño responsive (puedes usar Bootstrap) una web que muestre unas 7 fichas de personajes (o clases) 
-    de un videojuego. Ten en cuenta que toda la información mostrada debe salir de un array. 
-    Te recorres el array y muestras el resultado. Decide la información a mostrar, puedes usar alguna web de 
-    ejemplo e imitar el estilo. Ten en cuenta también que cada elemento de un array puede ser a su vez un array. 
-    Por ejemplo, para añadir varias características de un personaje, o varias fotos.*/
-    ?>
+
+
+    <div class="container">
+
+        <?php
+        /*Web videojuego. 
+        Intenta hacer con diseño responsive (puedes usar Bootstrap) una web que muestre unas 7 fichas de personajes (o clases) 
+        de un videojuego. Ten en cuenta que toda la información mostrada debe salir de un array. 
+        Te recorres el array y muestras el resultado. Decide la información a mostrar, puedes usar alguna web de 
+        ejemplo e imitar el estilo. Ten en cuenta también que cada elemento de un array puede ser a su vez un array. 
+        Por ejemplo, para añadir varias características de un personaje, o varias fotos.*/
+
+        function pintar($productos)
+        {
+            foreach ($productos as $valor) {
+                echo "<div class='card mb-3' style='max-width: 540px;'>
+                                <div class='row g-0'>
+                                  <div class='col-md-4'>
+                                    <img src='".$valor["imagen"]."' class='img-fluid rounded-start' alt='...' width=250px height=300px>
+                                  </div>
+                                  <div class='col-md-8'>
+                                    <div class='card-body'>
+                                      <h5 class='card-title'>Card title</h5>
+                                      <p class='card-text'>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                      <p class='card-text'><small class='text-muted'>Last updated 3 mins ago</small></p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>";
+
+                /*Pintar las tres imágenes*/
+                echo "<table class='table table-bordered blue-500'>";
+                echo "<tr>";
+                foreach ($valor['imagenes'] as $imagenMini) {
+                    echo "<td>";
+                    echo "<img src='". $imagenMini ." 'alt='...' width='40'>";
+                    echo "</td>";
+                }
+                echo "</tr>";
+                echo "</table>"; 
+
+               
+            }
+        }
+
+
+        //Productos de una tienda
+        $productos = array(
+            array("clase" => "barbaro", "descripcion" => "desc", "habilidades" => "1,2,3..", "imagen" => "img/erron.png", "imagenes" => array("img/1.jpg","img/1.jpg","img/1.jpg","img/1.jpg")),
+            array("clase" => "barbaro", "descripcion" => "desc", "habilidades" => "1,2,3..", "imagen" => "img/scorpion.png"),
+            array("clase" => "barbaro", "descripcion" => "desc", "habilidades" => "1,2,3..", "imagen" => "img/cassie.png")
+        );
+        /*
+        //Pintar productos
+        echo "<table class='table table-striped table-hover'>";
+        echo "<thead class='text-primary'>";
+        echo "  <tr>";
+        //Sacamos el nombre de cada columna con array_keys del primer array (producto)
+        foreach(array_keys($productos[0]) as $valor)
+            echo "<td>".strtoupper($valor)."</td>";
+        echo "  </tr>";
+        echo "</thead>";
+        echo "<tbody>";
+        foreach($productos as $valor) {
+            echo "<tr>";
+            foreach($valor as $campo) {
+                echo "<td>";
+                echo $campo;
+                echo "</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</tbody>";
+        echo "</table>";
+        echo "<br><br>";
+        */
+
+
+        echo "<div class='container'>";
+        echo "<div class='row'>";
+
+        pintar($productos);
+
+
+
+
+        echo "</div>";
+        echo "</div>";
+
+
+
+        ?>
 </body>
+
 </html>
