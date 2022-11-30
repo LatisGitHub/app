56d8ba70644798aa5d1825c94f91cab988a53018
LatisGitHub <latischannel2@gmail.com>
2022-11-30 15:16:27 +0100

    cabera

--- tema5/biblioteca-Objetos/cabecera2.php
+++ tema5/biblioteca-Objetos/cabecera2.php
@@ -19,7 +19,7 @@ session_start();
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="plantilla/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -33,7 +33,6 @@ session_start();
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="enrutador.php?accion=inicio">
-                <img src="../imgs/libro.png" alt="">
                 <div class="sidebar-brand-text mx-3">LIBRERIA</div>
             </a>
 
